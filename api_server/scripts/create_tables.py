9c1e06b3d0ea9c8116c29e605b7aef622aa2822e
Yartsev Vladimir <v.rainovsky@yandex.ru>
2024-02-12 16:42:51 +0300

    Добавлено создание перочальной записи

--- api_server/scripts/create_tables.py
+++ api_server/scripts/create_tables.py
@@ -16,17 +16,22 @@ def create_tables():
 def fill_first_data():
     init_database()
     User.create(login='admin', password=auth.encode_password("admin"), surname="Администратор", name='',middle_name='')
-    User.create(login='teacher', password=auth.encode_password("teacher"), surname="Тестов",
-                name='Препод', middle_name='Академиков')
+    User.create(login='teacher', password=auth.encode_password("teacher"), surname="Никонов",
+                name='Михаил', middle_name='Дмитриевич')
+    User.create(login='teacher2', password=auth.encode_password("teacher2"), surname="Михайленко",
+                name='Сергей', middle_name='Анатольевич')
 
     Post.create(name='Преподаватель')
     Post.create(name='Доцент')
+    Post.create(name='Профессор')
 
     Department.create(name='Кафедра ТиМ ФК')
     Department.create(name='Кафедра ТиМ ЦВС')
+    Department.create(name='Кафедра ТиМ СЕиТА')
 
     Staff.create(user_id=2, department_id=1,post_id=2)
     Staff.create(user_id=2, department_id=2, post_id=1)
+    Staff.create(user_id=3, department_id=3, post_id=3)
 
     close_database()
 
